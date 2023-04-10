b0ddc070b66a5e98173af2bcf60af4054c4b5954
pratyusha319 <pratyushagorrela8@gmail.com>
2023-04-10 19:52:06 +0530

    first commit

---
+++ dbase/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from dbase.models import *
+# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecords)
